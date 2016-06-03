b98d22b0ff4642948c308fd29f83229620510a15
Steffen Mecke <stm2@users.sf.net>
2016-06-03 16:00:23 +0200

    added dfv skin

---
+++ cust/dfv/head.php
@@ -0,0 +1,15 @@
+<?php
+function logo() {
+	global $styles_prefix;
+	global $include_prefix;
+	if (!isset($styles_prefix)) {
+		$styles_prefix = $include_prefix;
+	}
+	return "<img class='header_logo' src='".$styles_prefix."cust/dfv/logo.gif' alt='"._("EXAMPLE")."'/>";
+}
+
+function pageHeader() {
+	return "<a href='http://www.frisbeesportverband.de' class='header_text'>"._("Ultiorganizer")."</a><br/>\n";
+}
+
+?>
