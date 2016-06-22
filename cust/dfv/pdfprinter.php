85dfcb68c8d2fd87b6ea1ba400412f1bb26ca56d
Steffen Mecke <stm2@users.sf.net>
2016-06-22 11:35:39 +0200

    tweaks

--- cust/dfv/pdfprinter.php
+++ cust/dfv/pdfprinter.php
@@ -785,7 +785,7 @@ class PDF extends FPDF
 		$this->SetFont('Arial','B',12);
 		$this->SetTextColor(255);
 		$this->SetFillColor(0,0,0);
-		$this->Cell(80,6,utf8_decode(_("Timeouts")),'LRTB',0,'C',true);
+		$this->Cell(80,6,utf8_decode(_("Time-outs")),'LRTB',0,'C',true);
 		$this->Ln();
 		
 		//home grids
