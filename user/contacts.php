14eda976e43e01e9474feed8a08e5843450b25a9
Steffen Mecke <stm2@users.sf.net>
2018-01-29 23:59:31 +0100

    amend contacts page

--- user/contacts.php
+++ user/contacts.php
@@ -1,5 +1,5 @@
 <?php
-include_once $include_prefix.'lib/season.functions.php';
+include_once $include_prefix . 'lib/season.functions.php';
 
 $title = _("Contacts");
 $html = "";
@@ -9,89 +9,99 @@ if (empty($_GET['season'])) {
 }
 $season = $_GET['season'];
 $links = getEditSeasonLinks();
-if (!isset($links[$season]['?view=user/contacts&amp;season='.$season])) {
+if (!isset($links[$season]['?view=user/contacts&amp;season=' . $season])) {
   die(_("Inadequate user rights"));
 }
 
-$html .=  "<h2>"._("Contacts")."</h2>";
+$html .= "<h2>" . _("Contacts") . "</h2>\n";
 
+$resp = SeasonTeamAdmins($season, true);
+$html .= "<div><a href='" . mailto_encode($resp, 'email', 'name') . "'>" . _("Mail to everyone registered for the event") . "</a></div>";
 
-$resp = SeasonTeamAdmins($season,true);
-$html .=  "<div><a href='" . mailto_encode($resp, 'email', 'name'). "'>"._("Mail to everyone registered for the event")."</a></div>";
-
-$html .=  "<h3>"._("Mail to Event Organizers")."</h3>";
+$html .= "<h3>" . _("Mail to Event Organizers") . "</h3>\n";
 $admins = SeasonAdmins($season);
-$html .=  "<ul>";
+$html .= "<ul>";
 $all = "";
-foreach($admins as $user){
-  if(!empty($user['email'])){
-    $html .=  "<li> " . mailto_link($user['email'], $user['name']). "</li>\n";
-    $all .= mailto_address($user['email'], $user['name']).";";
+foreach ($admins as $user) {
+  if (!empty($user['email'])) {
+    $html .= "<li>" . mailto_link($user['email'], $user['name']) . "</li>\n";
+    $all .= mailto_address($user['email'], $user['name']) . ";";
   }
 }
-$html .= "<li><a href='mailto:".$all."'>". _("All organizers") ."</a></li>\n";
-$html .=  "</ul>\n";
+$html .= "<li><a href='mailto:" . $all . "'>" . _("All organizers") . "</a></li>\n";
+$html .= "</ul>\n";
 
-$html .=  "<h3>"._("Mail to Division Organizers")."</h3>";
+$html .= "<h3>" . _("Mail to Division Organizers") . "</h3>\n";
 $series = SeasonSeries($season);
-$html .=  "<ul>";
-foreach($series as $row){
-  $html .=  "<li><p><b>".utf8entities(U_($row['name']))."</b></p>";
-  $admins = SeriesAdmins($row['series_id']);
-  $html .=  "<ul>";
-  $all = "";
-  foreach($admins as $user){
-    if(!empty($user['email'])){
-      $html .=  "<li> " . mailto_link($user['email'], $user['name']) . " (".utf8entities($user['name']) . ")</li>\n";;
-      $all .= mailto_address($user['email'], $user['name']).";";
-    }
-  }
-  if (empty($all)) {
-    $html .= "<li>" . _("No admins known") . "</li>\n";
-  } else {
-    $html .= "<li><a href='mailto:" . $all . "'>" . _("All admins") . "</a></li>\n";
-  }
-  $html .= "</ul></li>\n";
-}
-
-$admins = SeasonSeriesAdmins($season);
-$all = "";
-foreach($admins as $user){
-  if(!empty($user['email'])){
-    $all .= mailto_encode($user, 'email', 'name').";";
-  }
-}
-
-if (!empty($all)) {
-  $first = false;
-  $html .= "<li><a href='" . mailto_encode($admins, 'email', 'name') . "'>" . _("All season admins") ."</a></li>\n";
-}
-$html .= "</ul>";
-  
-$html .=  "<h3>"._("Mail to Teams")."</h3>";
-
-$series = SeasonSeries($season);
-foreach($series as $row){
-
-  $html .=  "<p><b>".utf8entities(U_($row['name']))."</b></p>";
-  $resp = SeriesTeamResponsibles($row['series_id']);
-  $html .=  "<div><a href='" . mailto_encode($resp, 'email', 'name') . "'>" 
-    . _("Mail to teams in")." ".U_($row['name']) . "</a></div>";
-
-  $teams = SeriesTeams($row['series_id']);
-  $html .=  "<ul>";
-  foreach($teams as $team){
-    $html .=  "<li>".utf8entities($team['name']).":";
-    $admins = GetTeamAdmins($team['team_id']);
-    foreach($admins as $user){
-      if(!empty($user['email'])){
-        $html .= mailto_link($user['email'], $user['name']);
-        // $html .=  " (".utf8entities($user['name']).")";
+$html .= "<ul>";
+$seasonAdmins = array();
+$numSeries = 0;
+foreach ($series as $row) {
+  if (hasEditSeriesRight($row['series_id'])) { /* FIXME necessary? */
+    $html .= "<li><b>" . utf8entities(U_($row['name'])) . "</b>\n";
+    $admins = SeriesAdmins($row['series_id']);
+    $html .= "  <ul>";
+    $all = "";
+    foreach ($admins as $user) {
+      if (!empty($user['email'])) {
+        $html .= "  <li>" . mailto_link($user['email'], $user['name']) . " (" . utf8entities($user['name']) . ")</li>\n";
+        $all .= mailto_address($user['email'], $user['name']) . ";";
+        $seasonAdmins[$user['email']] = $user;
       }
     }
-    $html .=  "</li>\n";
+    $html .= "</ul></li>\n";
+    if (empty($all)) {
+      $html .= "<li>" . _("No admins known") . "</li>\n";
+    } else {
+      $numSeries ++;
+      $html .= "<li><a href='mailto:" . $all . "'>" . _("All admins") . "</a></li>\n";
+    }
   }
-  $html .=  "</ul>\n";
+}
+
+if ($numSeries > 0) {
+  $first = false;
+  $all = "";
+  foreach ($seasonAdmins as $user) {
+    $all .= mailto_address($user['email'], $user['name']);
+  }
+  $html .= "<li><a href='mailto:" . $all . "'>" . _("All season admins") . "</a></li>";
+}
+
+$html .= "</ul>\n";
+
+$html .= "<h3>" . _("Mail to Teams") . "</h3>\n";
+
+$series = SeasonSeries($season);
+foreach ($series as $row) {
+  if (hasEditSeriesRight($row['series_id'])) {
+    $html .= "<p><b>" . utf8entities(U_($row['name'])) . "</b></p>\n";
+    
+    $teams = SeriesTeams($row['series_id']);
+    $html .= "<ul>";
+    foreach ($teams as $team) {
+      $html .= "<li>" . utf8entities($team['name']) . ": ";
+      $admins = GetTeamAdmins($team['team_id']);
+      if (empty($admins)) {
+        $html .= "---";
+      } else {
+        foreach ($admins as $user) {
+          if (!empty($user['email'])) {
+            $html .= mailto_link($user['email'], $user['name']);
+            // $html .= " (".utf8entities($user['name']).")";
+          }
+        }
+      }
+      
+      $html .= "</li>\n";
+    }
+    
+    $resp = SeriesTeamResponsibles($row['series_id']);
+    if (!empty($resp))
+      $html .= "<li><a href='" . mailto_encode($resp, 'email', 'name') . "'>" . _("Mail to teams in") . " " . U_($row['name']) . "</a></li>";
+    
+    $html .= "</ul>\n";
+  }
 }
 
 showPage($title, $html);
