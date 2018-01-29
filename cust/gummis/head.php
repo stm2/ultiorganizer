b71dc6ebf0b92c1d5568c8096e582d629c85cf1c
Steffen Mecke <stm2@users.sf.net>
2018-01-30 00:07:06 +0100

    tweaked gummis layout

--- cust/gummis/head.php
+++ cust/gummis/head.php
@@ -9,7 +9,7 @@ function pageHeader() {
 	if (!isset($styles_prefix)) {
 		$styles_prefix = $include_prefix;
 	}
-	return "<a href='http://www.ultimate-karlsruhe.de' class='header_text'><img class='header_logo' style='width:550px;height:50px' src='".$styles_prefix."cust/gummis/header.png' alt='Gummibärchen Karlsruhe'/></a><br/>\n";
+	return "<a href='http://www.ultimate-karlsruhe.de' class='header_text'><img class='header_logo' src='".$styles_prefix."cust/gummis/header_long.png' alt='Gummibärchen Karlsruhe'/></a><br/>\n";
 }
 
 ?>
