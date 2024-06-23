a0a992fbe210e7dead5423b2b0d9a7bfc975e78f
Steffen Mecke <stm2@users.sf.net>
2024-06-23 18:45:51 +0200

    fixed results listing for REST interface

--- ext/restful/results.php
+++ ext/restful/results.php
@@ -5,7 +5,7 @@ class Results extends Restful {
   var $listfields;
 
   function __construct() {
-    $this->listsql = "SELECT game.game_id AS id, '' AS name, game.time AS 'time', 
+    $this->listsql = "SELECT game.game_id AS id, '' AS name, game.game_id AS game, game.time AS 'time', 
                game.hometeam AS hometeam, hteam.name as hname,  
                game.visitorteam visitorteam, vteam.name as vname,
 			homescore AS 'homescore', visitorscore AS 'visitorscore',
@@ -21,7 +21,25 @@ class Results extends Restful {
                LEFT JOIN uo_season season on (season.season_id=series.season)
 			LEFT JOIN uo_team hteam on (game.hometeam=hteam.team_id)
 			LEFT JOIN uo_team vteam on (game.visitorteam=vteam.team_id)";
-
+    
+    $this->itemsql = "SELECT game.game_id AS id, game.game_id AS game, '' AS name, game.time AS 'time',
+               game.hometeam AS hometeam, hteam.name as hname,
+               game.visitorteam visitorteam, vteam.name as vname,
+			homescore AS 'homescore', visitorscore AS 'visitorscore',
+               game.pool AS pool,
+               pool.name AS poolname,
+               pool.series AS series,
+               series.name AS seriesname,
+			series.season AS season,
+               season.name AS seasonname
+            FROM uo_game game
+               LEFT JOIN uo_pool pool on (game.pool=pool.pool_id)
+			LEFT JOIN uo_series series on (series.series_id=pool.series)
+               LEFT JOIN uo_season season on (season.season_id=series.season)
+			LEFT JOIN uo_team hteam on (game.hometeam=hteam.team_id)
+			LEFT JOIN uo_team vteam on (game.visitorteam=vteam.team_id)
+            WHERE game.game_id = '%s'";
+    
     $this->tables = array("uo_game" => "game", "uo_pool" => "pool", "uo_series" => "series", "uo_season" => "season",
       "uo_team" => "team");
 
@@ -37,14 +55,17 @@ class Results extends Restful {
         array("field" => "season.iscurrent", "operator" => "=", "value" => 1),
         array("field" => "series.valid", "operator" => "=", "value" => 1)));
 
-    // $this->linkfields["hometeam"] = "teams";
-    // $this->linkfields["visitorteam"] = "teams";
-    // $this->linkfields["pool"] = "pools";
-    // $this->linkfields["series"] = "series";
-    // $this->linkfields["season"] = "seasons";
+    $this->linkfields["game"] = "games";
+    $this->linkfields["hometeam"] = "teams";
+    $this->linkfields["visitorteam"] = "teams";
+    $this->linkfields["pool"] = "pools";
+    $this->linkfields["series"] = "series";
+    $this->linkfields["season"] = "seasons";
 
-    // $this->listfields = array('time', 'hometeam' => name, 'visitorteam', 'homescore', 'visitorscore', 'pool', 'series', 'season');
-    $this->listfields = array('time', 'hometeam' => array("id" => "hometeam", "name" => 'hname', 'link' => "teams"),
+//     $this->listfields = array('time', 'hometeam' => 'name', 'visitorteam', 'homescore', 'visitorscore', 'pool', 'series', 'season');
+    $this->listfields = 
+    array('game' => [ 'id' => "game", 'link' => 'games'],
+      'time', 'hometeam' => array("id" => "hometeam", "name" => 'hname', 'link' => "teams"),
       'visitorteam' => array("id" => "visitorteam", "name" => 'vname', 'link' => "teams"), 'homescore', 'visitorscore',
       'pool' => array("id" => "pool", "name" => 'poolname', 'link' => "pools"),
       'series' => array("id" => "series", "name" => 'seriesname', 'link' => "series"),
@@ -70,7 +91,8 @@ class Results extends Restful {
       }
     }
     unset($ret['link']);
-    $this->convertLinkFields($ret, false);
+    
+//     $this->convertLinkFields($ret, false);
     return $ret;
   }
 
